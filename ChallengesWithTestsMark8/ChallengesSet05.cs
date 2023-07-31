3a7caccac32da8fc612ba4d331f7b93d2dd7ab4e
Haroutun Panosyan <harupanosyan@gmail.com>
2023-07-31 16:28:56 -0700

    Set 5 and 6 complete

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +8,143 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            if (++startNumber % n == 0)
+                return startNumber;
+            else
+            {
+                while (startNumber % n != 0)
+                {
+                    startNumber++;
+                }
+                return startNumber;
+            }
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            Business business = new Business();
+            foreach (var biz in businesses)
+            {
+                if (biz.TotalRevenue == 0)
+                {
+                    biz.Name = "CLOSED";
+                }
+            }
+            
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            // Check if array is null.
+            if (numbers == null || !numbers.Any())
+            {
+                return false;
+            }
+            // Check if numbers is ascending order.
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                // if starting index is greater than next index, return false.
+                if (numbers[i - 1] > numbers[i])
+                {
+                    return false;
+                }
+            }
+            // If everything else is false, then must be true.
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            // Null Checker
+            if (numbers == null || !numbers.Any())
+            {
+                return 0;
+            }
+            // Create integer variable named sum and set it to 0;
+            int sum = 0;
+            // In the case that the index of numbers prior to current index is divisible by 2 (or is even)
+            // add it to and set the new number to sum.
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            // Null Checker
+            if (words == null || !words.Any())
+            {
+                return "";
+            }
+            // Create a variable of type string that is empty. 
+            string sentence = string.Empty;
+            // Using a for loop to iterate through each index of the string array.
+            for (int i = 0; i < words.Length;)
+            {
+                // Check if current index + 1 is less than the array length,
+                // this is to match the starting length.              
+                if (i + 1 < words.Length)
+                {
+                    // If word at index i is empty, just iterate forward and add nothing.
+                    if (words[i].Trim() == "")
+                    {
+                        i++;
+                    }
+                    // Otherwise, add word at current index with white space trimmed.
+                    // The add a space to make it look like a normal sentence.
+                    sentence += words[i].Trim() + " ";
+                    i++;
+                }         
+                // Otherise add the word to sentence with a trim, but no space afterwards.
+                // Iterate forward.
+                else
+                {
+                    sentence += words[i].Trim();
+                    i++;
+                }
+            }
+            // Test to see if the sentence has anything in it. If not, return a blank (or empty string).
+            if (sentence == " ") 
+            {
+                return string.Empty;
+            }
+            // Otherwise, return the full sentence with a period at the end.
+            return sentence + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
-        {
-            throw new NotImplementedException();
+        {            
+            List<double> newElements = new List<double>();
+            if (elements == null || !elements.Any())
+            {
+                return newElements.ToArray();
+            }
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % 4 == 0)
+                newElements.Add(elements[i]);
+            }
+            return newElements.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] + nums[j] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +9,143 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            #region WithHelpFromSeth
+            //bool containsWord = false;
+            //if (string.Equals(words, null) || words.Contains(null))
+            //{
+            //    return false;
+            //}
+            //if (ignoreCase)
+            //{
+            //    word = word.ToLower();
+            //    List<string> list = words.Select(x => x.ToLower()).ToList();
+            //    containsWord = list.Contains(word);
+            //}
+            //if (!ignoreCase)
+            //{
+            //    containsWord = words.Contains(word);                
+            //}
+            //return containsWord;
+            #endregion
+            #region MyFirstAttempt
+            //if (word != null & words?.Any(w => w.ToLower().Contains(word)) == true & ignoreCase == true)
+            //{
+            //    return true;
+            //}
+            //else if (word != null & words?.Any(w => w.Contains(word)) == true & ignoreCase == false)
+            //{
+            //    return true;
+            //}
+            //else
+            //    return false;
+            #endregion
+            #region HelpFromChatGPT
+            if (word == null || words == null)
+                return false;
+            words = words.Where(w => w != null);
+
+            if (ignoreCase)
+            {
+                return words.Any(w => w.ToLower().Contains(word.ToLower()));
+            }
+            else
+            {
+                return words.Any(w => w.Contains(word));
+            }
+            #endregion
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= num; i++)
+            {
+                if (num == 1) return false;
+                if (num % i == 0 && i != 1 & i != num)
+                {
+                    break;
+                }
+                if (i / 1 == num && i / num == 1)
+                {
+                    return true;
+                }                    
+            }
+            return false;
+            #region Test1
+            //if (num == 2)
+            //{
+            //    return true;
+            //}
+            //if (num == 1)
+            //{
+            //    return false;
+            //}
+            //else if (num % 2 == 1 ||  num % 3 == 1 || num % 5 == 1 || num % 7 == 1 || num % 9 == 1 || num % 11 == 1 || num % 13 == 1)
+            //{                
+            //    return true;
+            //}
+            //return false;
+            #endregion
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                if (str.Count(x => x == str[i]) == 1)
+                {
+                    return i;
+                }                
+            }
+            return -1;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 1;
+            int retCount = 1;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int nextNum = numbers[i + 1];
+                int currentNum = numbers[i];
+                if (currentNum == nextNum)
+                {
+                    count++;
+                }                
+                if (retCount < count)
+                {
+                    retCount = count;
+                }
+                if (currentNum != nextNum)
+                {
+                    count = 1;
+                }
+            }
+            return retCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            double[] resArr = new double[0];
+            if (elements == null)
+            {
+                return resArr;
+            }
+            if (elements.Count() > 0 && n > 0)
+            {
+                List<double> results = new List<double>();
+                foreach (double elem in elements)
+                {
+                    if (elem % n == 0)
+                    {
+                        results.Add(elem);
+                    }
+                }
+                return results.ToArray();
+            }
+            else 
+            {                
+                return resArr;
+            }
         }
     }
 }
